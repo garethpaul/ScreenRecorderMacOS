6d123eedaa98f8694aff52ff2d93d4486782fcac
lukasg8 <lukasg@uchicago.edu>
2023-07-13 14:39:03 +0200

    added audio recorder class

--- CaptureSample/Record.swift
+++ CaptureSample/Record.swift
@@ -3,6 +3,101 @@ import SwiftUI
 import Foundation
 import AVFoundation
 
+class AudioRecorder {
+    
+    private var assetWriter: AVAssetWriter?
+    
+    private var assetWriterAudioInput: AVAssetWriterInput?
+    
+    private var audioSettings: [String: Any]
+    
+    // can be read from anywhere (getter) but only modified in this class (setter)
+    private(set) var isRecording = false
+    
+    init(audioSettings: [String:Any]) {
+        self.audioSettings = audioSettings
+    }
+    
+    private func documentDirectory() -> String {
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                                    .userDomainMask,
+                                                                    true)
+        return documentDirectory[0]
+    }
+    
+    private func append(toPath path: String,
+                        withPathComponent pathComponent: String) -> String? {
+        if var pathURL = URL(string: path) {
+            pathURL.appendPathComponent(pathComponent)
+
+            return pathURL.absoluteString
+        }
+
+        return nil
+    }
+    
+    func startRecording(height: Int, width: Int) {
+        let outputFileName = NSUUID().uuidString
+        let filePath = self.append(toPath: self.documentDirectory(),
+                                             withPathComponent: outputFileName)
+        let outputFileURL = URL(fileURLWithPath: filePath!).appendingPathExtension("WAV")
+        guard let assetWriter = try? AVAssetWriter(url: outputFileURL, fileType: .wav) else {
+            return
+        }
+
+        // Add an audio input
+        let audioSettings = [
+                    AVFormatIDKey: kAudioFormatLinearPCM,
+                    AVSampleRateKey: 44100,
+                    AVNumberOfChannelsKey: 2,
+                    AVLinearPCMBitDepthKey: 16,
+                    AVLinearPCMIsNonInterleaved: false,
+                    AVLinearPCMIsFloatKey: false,
+                    AVLinearPCMIsBigEndianKey: false
+                ] as [String : Any]
+
+        let assetWriterAudioInput = AVAssetWriterInput(mediaType: .audio, outputSettings: audioSettings)
+        assetWriterAudioInput.expectsMediaDataInRealTime = true
+        assetWriter.add(assetWriterAudioInput)
+
+        self.assetWriter = assetWriter
+        self.assetWriterAudioInput = assetWriterAudioInput
+
+        isRecording = true
+    }
+
+    func stopRecording(completion: @escaping (URL) -> Void) {
+        guard let assetWriter = assetWriter else {
+            return
+        }
+
+        self.isRecording = false
+        self.assetWriter = nil
+
+        assetWriter.finishWriting {
+            completion(assetWriter.outputURL)
+        }
+    }
+    
+    func recordAudio(sampleBuffer: CMSampleBuffer) {
+        guard isRecording,
+            let assetWriter = assetWriter else {
+                return
+        }
+
+        if assetWriter.status == .unknown {
+            assetWriter.startWriting()
+            assetWriter.startSession(atSourceTime: CMSampleBufferGetPresentationTimeStamp(sampleBuffer))
+        } else if assetWriter.status == .writing {
+            if let input = assetWriterAudioInput,
+                input.isReadyForMoreMediaData {
+                input.append(sampleBuffer)
+            }
+        }
+    }
+    
+}
+
 class MovieRecorder {
 
     private var assetWriter: AVAssetWriter?
